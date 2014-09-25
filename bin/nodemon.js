d6f10eacba667b89b45dfaa9215b20d455666d25
Artem Nezvigin <artem@artnez.com>
2014-09-25 00:05:14 -0700

    Drop update-notifier support

--- bin/nodemon.js
+++ bin/nodemon.js
@@ -1,15 +1,7 @@
 #!/usr/bin/env node
 'use strict';
 var cli = require('../lib/cli'),
-    nodemon = require('../lib/'),
-    updateNotifier = require('update-notifier'),
-    // checks for available update and returns an instance
-    notifier = updateNotifier({ packagePath: '../package' });
-
-if (notifier.update) {
-  // notify using the built-in convenience method
-  notifier.notify();
-}
+    nodemon = require('../lib/');
 
 var options = cli.parse(process.argv);
 
